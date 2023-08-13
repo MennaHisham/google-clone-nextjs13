fb452ff70cc125e23152b8ce33fea6b18d1bd55b
MennaHisham <mennahishamh@gmail.com>
2023-08-13 17:23:41 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimenal:{
+        appDir: true
+    },
+    images: {
+        domains:["upload.wikimedia.org"]
+    }
+}
+
+module.exports = nextConfig
