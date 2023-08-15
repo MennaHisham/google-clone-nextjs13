53b89a9ee16372a230e4fbe7579cd7c77ace86b0
MennaHisham <mennahishamh@gmail.com>
2023-08-15 15:02:33 +0300

    fix searchParam for ferching data from google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,9 @@
-export default async function WebSearchPage(searchParams) {
+export default async function WebSearchPage({searchParams}) {
 
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
+
   const data = await response.json();
 
   const results = data.items;
