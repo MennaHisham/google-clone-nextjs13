72a2015972a6af3d3e31d58f375ccf535574e802
MennaHisham <mennahishamh@gmail.com>
2023-08-15 14:57:30 +0300

    fetch data from google search api and show the titles

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     },
     images: {
         domains:["upload.wikimedia.org"]
+    },
+    env: {
+        NEXT_PUBLIC_GOOGLE_API_KEY:'AIzaSyBVKSEVlc97OstT7KMn8ELFE4_LMmM84fM',
+        NEXT_PUBLIC_CONTEXT_KEY:'e30eca39f01794ee1'
     }
 }
 
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+export default async function WebSearchPage(searchParams) {
 
-export default function WebSearchPage() {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchParams.searchTerm}`
+  )
+  const data = await response.json();
+
+  const results = data.items;
+
   return (
-    <div>
-      WebSearchPage
-    </div>
+    <>
+    {results && results.map((result)=> <h1>{result.title}</h1>)}
+    </>
   )
 }
