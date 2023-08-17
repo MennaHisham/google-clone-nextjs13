c2a385c70924f3a0c2f8f4c54499ccc5e52d38f8
MennaHisham <mennahishamh@gmail.com>
2023-08-17 16:54:45 +0300

    create loading page for web page results

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,24 @@
+export default function loading() {
+  return (
+    <>
+    <div className="max-w-6xl pt-10 mx-2 lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 w-48 "></div>
+        <div className="h-3.5 w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+    </div>
+    <div className="max-w-6xl pt-10 mx-2 lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 w-48 "></div>
+        <div className="h-3.5 w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+    </div>
+    <div className="max-w-6xl pt-10 mx-2 lg:pl-52 animate-pulse">
+        <div className="bg-gray-200 rounded-full mb-2.5 h-2.5 w-48 "></div>
+        <div className="h-3.5 w-[360px] bg-gray-200 rounded-full mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[560px]"></div>
+        <div className="h-2 bg-gray-200 rounded-full mb-2.5 max-w-[530px]"></div>
+    </div>
+    </>
+  )
+}
