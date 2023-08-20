c952ad31bf25ae39e9254f523181c09d37a95c2a
MennaHisham <mennahishamh@gmail.com>
2023-08-20 11:23:52 +0300

    edit next config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,7 @@ const nextConfig = {
     },
     images: {
         domains:["upload.wikimedia.org"]
-    },
-    env: {
-        NEXT_PUBLIC_GOOGLE_API_KEY:'AIzaSyBVKSEVlc97OstT7KMn8ELFE4_LMmM84fM',
-        NEXT_PUBLIC_CONTEXT_KEY:'e30eca39f01794ee1'
     }
-}
+    }
 
 module.exports = nextConfig
